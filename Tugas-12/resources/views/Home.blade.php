b6b9ff6a0859436dea3c4beb09e2f1eda856cb5c
widhiersa <widhi12315@gmail.com>
2023-06-07 04:37:57 +0700

    commit ke-8
    Intro Laravel (Instalasi dan Membuat Web Statis)

---
+++ Tugas-12/resources/views/Home.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Index Form</title>
+</head>
+<body>
+  <h1>Garuda Cyber Institute </h1>
+  <h2>Jadilah Programmer Yang Handal </h2>
+  <h2>Syarat dan Ketentuan</h2>
+  <ul type="disc">
+    <li>Tamatan SMA/SMK</li>
+    <li>Tamatan Perguruan Tinggi</li>
+    <li>Pekerja IT</li>
+    <li>Freelancer</li>
+    <li>berperilaku jujur</li>
+</ul>
+  <h2>Cara Bergabung</h2>
+  <ol>
+    <li>Kunjungi website GC</li>
+    <li><a href="register">Register</a></li>
+</ol>
+</body>
+</html>
---
+++ Tugas-12/resources/views/Register.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tugas laravel</title>
+</head>
+<body>
+    <!--Membuat Form-->
+    <h1>Buat Account Baru!</h1>
+    <h3>Sign Up Form</h3>
+    <form action="/welcome" method="post">
+        @csrf
+        <label >First_name</label><br><br>
+        <input type="text" name="fn"><br><br>
+        <label >Last_name</label><br><br>
+        <input type="text" name="ln"><br><br>
+        <label >Gender:</label> <br><br>
+        <input type="radio" name="gender">Male <br>
+        <input type="radio" name="gender">Female <br>
+        <input type="radio" name="gender">Other <br><br>
+        <label >Nationality:</label><br><br>
+        <select name="nationality" >
+            <option value="indonesia">Indonesia</option>
+            <option value="malaysia">Malaysia</option>
+            <option value="singapura">Singapura</option>
+        </select> <br><br>
+        <label >Language Spoken:</label><br><br>
+        <input type="checkbox" name="lp">Bahasa Indonesia <br>
+        <input type="checkbox" name="lp">English<br>
+        <input type="checkbox" name="lp">Other<br><br>
+        <label>Bio</label><br>
+        <textarea name="bio" cols="30" rows="10"></textarea><br>
+        <input type="submit" value="Sign Up">
+    </form>
+
+</body>
+</html>
